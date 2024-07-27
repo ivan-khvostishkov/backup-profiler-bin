27b93ad4974431df3c89f7e9f6d79755fcef20c7
Ivan Khvostishkov <87804596+ivan-khvostishkov@users.noreply.github.com>
2024-07-27 16:35:16 +0200

    Incremental init - placeholder

--- init-incremental.sh
+++ init-incremental.sh
@@ -2,17 +2,19 @@
 
 ### Use this file as a template and copy to your backup dir, e.g., to `../2024-08-01.hostname.incremental/`
 
-echo 'Initializing Backup Profiler v3.0'
+echo 'Initializing Backup Profiler v3.0 - Incremental run'
 
 
 # 1. Copy files from previous backup for incremental profile
 
 mkdir -p ./important/count/ ./important/size/ ./important/time/ ./unimportant/count/ ./unimportant/size/
 
-cp -v ../2024-01-02.hostname/important/count/profile-ack.txt ./important/count/ 
-cp -v ../2024-01-02.hostname/important/size/profile-ack.txt ./important/size/ 
-cp -v ../2024-01-02.hostname/important/time/profile-ack.txt ./important/time/ 
-cp -v ../2024-01-02.hostname/unimportant/count/profile-ack.txt ./unimportant/count/ 
-cp -v ../2024-01-02.hostname/unimportant/size/profile-ack.txt ./unimportant/size/ 
+previous='../2024-05-01.hostname/'
+
+cp -v ../$previous/important/count/profile-ack.txt ./important/count/ 
+cp -v ../$previous/important/size/profile-ack.txt ./important/size/ 
+cp -v ../$previous/important/time/profile-ack.txt ./important/time/ 
+cp -v ../$previous/unimportant/count/profile-ack.txt ./unimportant/count/ 
+cp -v ../$previous/unimportant/size/profile-ack.txt ./unimportant/size/ 
 
 
